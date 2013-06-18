fdeb861d7a10b0e96ed33bf4b71ed9c1696d4fc3
Yixi <lyh1112@gmail.com>
2013-06-18 23:40:35 +0800

    ignore

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
